1faf2aa8c939e87386a7d12a1e212f3e077e8033
DanielSatzinger <satzingerdaniel12@gmail.com>
2024-09-02 11:37:25 +0200

    Delete backend-server directory

--- backend-server/socketio_instance.py
+++
@@ -1,4 +0,0 @@
-from flask_socketio import SocketIO
-
-# socketio = SocketIO(app, cors_allowed_origins="http://localhost:3000", logger=True, engineio_logger=True)
-socketio = SocketIO(cors_allowed_origins="*", logger=True, engineio_logger=True)
